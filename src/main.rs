7da0b69f7c100cdfc04b831340d1dae71eccd501
FloncDev <floncdev@gmail.com>
2023-10-22 01:21:53 +0100

    :construction: Add CLI structure
    No functionality as of yet - just defining the CLI with clap

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,54 @@
+use clap::{value_parser, Arg, ArgAction, Command};
+
 pub mod models;
 
+fn cli() -> Command {
+    Command::new("td")
+        .about("A simple todo app written in rust.")
+        .subcommand_required(true)
+        .arg_required_else_help(true)
+        .subcommand(
+            Command::new("list")
+                .about("List all todos. -a will also show completed todos.")
+                .arg(
+                    Arg::new("all")
+                        .short('a')
+                        .long("all")
+                        .action(ArgAction::SetTrue)
+                        .required(true)
+                ),
+        )
+        .subcommand(
+            Command::new("new").about("Create a new todo").arg(
+                Arg::new("summary")
+                    .short('s')
+                    .long("summary")
+                    .value_parser(value_parser!(String))
+                    .required(true)
+            ),
+        )
+        .arg(
+            Arg::new("description")
+                .short('d')
+                .long("desc")
+                .value_parser(value_parser!(String)),
+        )
+        .subcommand(
+            Command::new("done").about("Toggle done on a todo").arg(
+                Arg::new("id")
+                    .value_parser(value_parser!(u32))
+                    .required(true),
+            ),
+        )
+}
+
 fn main() {
-    println!("Hello, world!");
+    let matches = cli().get_matches();
+
+    match matches.subcommand() {
+        Some(("new", cmd_match)) => {
+            println!("{:#?}", cmd_match.get_one::<String>("summary").unwrap());
+        },
+        _ => {println!("other")}
+    }
 }
