29c5ba0c5f76d6e86dc44a6d2afce2809ef646d2
FloncDev <floncdev@gmail.com>
2023-10-24 23:39:27 +0100

    :sparkles: Finish basic CLI
    Added all commands needed for a basic todo list! (Even includes pretty colours)

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 use clap::{value_parser, Arg, ArgAction, Command};
+use crate::models::{Todo, Todos};
+use uuid::Uuid;
+use colored::Colorize;
 
 pub mod models;
 
@@ -15,7 +18,7 @@ fn cli() -> Command {
                         .short('a')
                         .long("all")
                         .action(ArgAction::SetTrue)
-                        .required(true)
+                        .required(false)
                 ),
         )
         .subcommand(
@@ -25,18 +28,25 @@ fn cli() -> Command {
                     .long("summary")
                     .value_parser(value_parser!(String))
                     .required(true)
-            ),
-        )
-        .arg(
-            Arg::new("description")
+            )
+            .arg(
+                Arg::new("description")
                 .short('d')
                 .long("desc")
                 .value_parser(value_parser!(String)),
+            )
         )
         .subcommand(
             Command::new("done").about("Toggle done on a todo").arg(
                 Arg::new("id")
-                    .value_parser(value_parser!(u32))
+                    .value_parser(value_parser!(String))
+                    .required(true),
+            ),
+        )
+        .subcommand(
+            Command::new("rm").about("Remove a todo").arg(
+                Arg::new("id")
+                    .value_parser(value_parser!(String))
                     .required(true),
             ),
         )
@@ -45,9 +55,49 @@ fn cli() -> Command {
 fn main() {
     let matches = cli().get_matches();
 
+    let mut todos = Todos::new();
+
     match matches.subcommand() {
         Some(("new", cmd_match)) => {
-            println!("{:#?}", cmd_match.get_one::<String>("summary").unwrap());
+            todos.add(Todo {
+                summary: cmd_match.get_one::<String>("summary").unwrap().to_owned(),
+                description: cmd_match.get_one::<String>("description").cloned(),
+                done: false,
+                id: Uuid::new_v4(),
+            }).unwrap();
+        },
+        Some(("list", cmd_match)) => {
+            let user_todos =
+                todos.todos
+                .iter()
+                .filter(
+                    |todo|
+                    !todo.done ||
+                    todo.done == cmd_match.get_flag("all")
+                );
+            
+            for user_todo in user_todos {
+                let done = if &user_todo.done == &true { "DONE".green() } else { "".normal() };
+                let description = user_todo.clone().description.unwrap_or(String::from(""));
+
+                println!(
+                    "\n{}\n",
+                    format!(
+                        "{}\n{} {}\n{}",
+                        format!("{}", user_todo.id).bright_black(),
+                        user_todo.summary.bold(),
+                        done,
+                        description
+                    ).trim()
+                );
+            }
+        },
+        Some(("done", cmd_match)) => {
+            let partial_id = cmd_match.get_one::<String>("id").unwrap();
+            todos.done(partial_id).unwrap();
+        },
+        Some(("rm", cmd_match)) => {
+            let partial_id = cmd_match.get_one::<String>("id").unwrap();
         },
         _ => {println!("other")}
     }
